2d9288637de0ee34b330dc299352be6a74a50c3d
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2016-01-13 22:51:53 +0200

    Implemented v1 of ScriptCs.CompileOnlyOption package.

---
+++ src/ScriptCs.CompileOnlyOption/CompileOnlyOptionFilePreProcessor.cs
@@ -0,0 +1,44 @@
+﻿namespace ScriptCs.CompileOnlyOption
+{
+    using System;
+    using System.Collections.Generic;
+
+    using ScriptCs.Contracts;
+
+    using ILog = Common.Logging.ILog;
+
+    public class CompileOnlyOptionFilePreProcessor : FilePreProcessor
+    {
+        private const string CompileOnlyArgumentName = "compile-only";
+
+        [Obsolete]
+        public CompileOnlyOptionFilePreProcessor(
+            IFileSystem fileSystem,
+            ILog logger,
+            IEnumerable<ILineProcessor> lineProcessors)
+            : base(fileSystem, logger, lineProcessors)
+        {
+        }
+
+        public CompileOnlyOptionFilePreProcessor(
+            IFileSystem fileSystem,
+            ILogProvider logProvider,
+            IEnumerable<ILineProcessor> lineProcessors)
+            : base(fileSystem, logProvider, lineProcessors)
+        {
+        }
+
+        protected override FilePreProcessorResult Process(Action<FileParserContext> parseAction)
+        {
+            var result = base.Process(parseAction);
+
+            result.Code =
+$@"if (Env.ScriptArgs.All(arg => !string.Equals(arg, ""{CompileOnlyArgumentName}"", StringComparison.OrdinalIgnoreCase)))
+{{
+    {result.Code}
+}}";
+
+            return result;
+        }
+    }
+}
---
+++ src/ScriptCs.CompileOnlyOption/CompileOnlyOptionModule.cs
@@ -0,0 +1,20 @@
+﻿namespace ScriptCs.CompileOnlyOption
+{
+    using System;
+
+    using ScriptCs.Contracts;
+
+    [Module("CompileOnlyOption", Extensions = "csx")]
+    public class CompileOnlyOptionModule : IModule
+    {
+        public void Initialize(IModuleConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+            
+            config.FilePreProcessor<CompileOnlyOptionFilePreProcessor>();
+        }
+    }
+}
